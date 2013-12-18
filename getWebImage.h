a7668d730ea3c579dd81768772a2892bc4266e38
D.A <KENTO@darekanoiMac.local>
2013-12-19 01:42:12 +0900

    add sample

---
+++ getWebImage.h
@@ -0,0 +1,20 @@
++(NSString*)sendImage:(UIImage*)_image url:(NSString*)url {
+    NSMutableURLRequest *request=[NSMutableURLRequest requestWithURL:[NSURL URLWithString:url] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:TIMEOUT_INTERVAL];
+    request.HTTPMethod=@"POST";
+    request.HTTPBody=[[NSString stringWithFormat:@"image=%@",[self image2string:_image]] dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error=nil;
+    NSData *response=[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&error];
+    if (error) {
+        NSLog(@"error to send image.");
+        UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"Communication error" message:@"Communication error...\nYou can enable wifi function." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+        return nil;
+    } else if (!response) {
+        NSLog(@"failed to send image.");
+        UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"Communication error" message:@"Communication failed...\nYou can enable wifi function." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+        return nil;
+    }
+    if (!response) return nil;
+    return [[NSString alloc] initWithData:response encoding:NSUTF8StringEncoding];
+}
